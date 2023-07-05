3c7d331fa930682a9bb7d1576f37e4ee568c0c27
wxb <superwang9527@163.com>
2023-07-06 00:28:06 +0800

    feat: rock game finished

--- src/components/rockGame/rockGame.tsx
+++ src/components/rockGame/rockGame.tsx
@@ -11,6 +11,10 @@ interface RockListItem {
   img: string;
 }
 
+interface GameOpponentListItem extends RockListItem {
+  result: ResultType;
+}
+
 const rockList: RockListItem[] = [
   {
     key: '0',
@@ -26,6 +30,52 @@ const rockList: RockListItem[] = [
   },
 ];
 
+type ResultType = 0 | 1 | 2;
+
+/**
+ * 石头剪刀布结果
+ * 0 输
+ * 1 赢
+ * 2 平
+ * @param key
+ * @param key1
+ * @returns
+ */
+const ruleHandler = (key: string, key1: string): ResultType => {
+  let result: ResultType = 2;
+  switch (key) {
+    case '0':
+      if (key1 === '0') {
+        result = 2;
+      } else if (key1 === '1') {
+        result = 1;
+      } else {
+        result = 0;
+      }
+      break;
+    case '1':
+      if (key1 === '1') {
+        result = 2;
+      } else if (key1 === '2') {
+        result = 1;
+      } else {
+        result = 0;
+      }
+      break;
+    case '2':
+      if (key1 === '2') {
+        result = 2;
+      } else if (key1 === '0') {
+        result = 1;
+      } else {
+        result = 0;
+      }
+      break;
+  }
+  console.log(key, typeof key, key1, typeof key1, result);
+  return result;
+};
+
 interface RockGameProps {
   isOpen: boolean;
   onChange(value: boolean): void;
@@ -35,6 +85,11 @@ const RockGame = (props: RockGameProps) => {
   const { isOpen, onChange } = props;
 
   const [selectedList, setSelectedList] = useState<RockListItem[]>([]);
+  const [gameOpponentList, setGameOpponentList] = useState<
+    GameOpponentListItem[]
+  >([]);
+  const [result, setResult] = useState(0);
+  const [thinking, setThinking] = useState(true);
 
   const selectedRock = (data: RockListItem) => {
     if (selectedList.length > 2) {
@@ -50,58 +105,168 @@ const RockGame = (props: RockGameProps) => {
     }
   };
 
+  const confirmHandler = () => {
+    const list = [];
+    const obj = {
+      '0': rock1,
+      '1': rock2,
+      '2': rock3,
+    };
+    let grade = 0;
+    for (let i = 0; i < 3; i++) {
+      const key = Math.floor(Math.random() * 3).toString();
+      const result = ruleHandler(selectedList[i].key, key);
+      if (result === 0) {
+        grade--;
+      } else if (result === 1) {
+        grade++;
+      }
+      list.push({
+        key,
+        img: obj[key as '0' | '1' | '2'],
+        result,
+      });
+    }
+    setResult(grade);
+    setGameOpponentList(list);
+    setThinking(false);
+  };
+
   return (
     <Modal isOpen={isOpen} className={styles.modalBox} width={600} height={600}>
       <div className={styles.gameBox}>
         <h1 className="title-1">决斗吧，骚年</h1>
         <p>规则：</p>
         <Typewriter text="选择每一轮的石头剪刀布，三局两胜!"></Typewriter>
-        <div className={styles.gameMain}>
-          <div className={styles.gameOpponentBox}>
-            <title className="text-green-500">
-              对方出招中
-              <span className="loading loading-dots loading-md"></span>
-            </title>
-          </div>
-          <div className={styles.imgListBox}>
-            {rockList.map((item) => (
-              <div
-                key={item.key}
-                className={styles.imgListItem}
-                style={{
-                  backgroundImage: `url(${item.img})`,
-                }}
-                onClick={() => selectedRock(item)}
-              ></div>
-            ))}
-          </div>
-          <div className={styles.resultBox}>
-            {['一', '二', '三'].map((item, index) => (
-              <div key={index} className={styles.resultItemBox}>
-                <title>{`第${item}局`}选择</title>
-                <div
-                  className={styles.resultItemImg}
-                  style={{
-                    backgroundImage: `url('${
-                      selectedList[index] ? selectedList[index].img : ''
-                    }')`,
-                  }}
-                ></div>
+        {thinking ? (
+          <>
+            <div className={styles.gameMain}>
+              <div className={styles.gameOpponentBox}>
+                <title className="text-green-500">
+                  隔壁老王出招中
+                  <span className="loading loading-dots loading-md"></span>
+                </title>
               </div>
-            ))}
+              <div className={styles.imgListBox}>
+                {rockList.map((item) => (
+                  <div
+                    key={item.key}
+                    className={styles.imgListItem}
+                    style={{
+                      backgroundImage: `url(${item.img})`,
+                    }}
+                    onClick={() => selectedRock(item)}
+                  ></div>
+                ))}
+              </div>
+              <div className={styles.resultBox}>
+                {['一', '二', '三'].map((item, index) => (
+                  <div key={index} className={styles.resultItemBox}>
+                    <title>{`第${item}局`}选择</title>
+                    <div
+                      className={styles.resultItemImg}
+                      style={{
+                        backgroundImage: `url('${
+                          selectedList[index] ? selectedList[index].img : ''
+                        }')`,
+                      }}
+                    ></div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className={styles.operationBox}>
+              <button
+                className="btn btn-outline btn-info"
+                onClick={goBackHandler}
+              >
+                撤回
+              </button>
+              <button
+                className="btn btn-outline btn-success"
+                onClick={confirmHandler}
+              >
+                确定
+              </button>
+              <button
+                className="btn btn-outline btn-error"
+                onClick={() => {
+                  onChange(false);
+                }}
+              >
+                投降
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className={styles.resultMain}>
+            <div className={`${styles.roleName} text-info`}>隔壁老王</div>
+            <div className={styles.resultBox}>
+              {gameOpponentList.map((item, index) => (
+                <div key={index} className={styles.resultItemBox}>
+                  <div
+                    className={styles.resultItemImg}
+                    style={{
+                      backgroundImage: `url('${item.img}')`,
+                    }}
+                  ></div>
+                  <div
+                    className={`${styles[`result${item.result}`]} ${
+                      item.result === 0
+                        ? 'text-error'
+                        : item.result === 1
+                        ? 'text-success'
+                        : 'text-warning'
+                    }`}
+                  >
+                    {item.result === 0 ? '输' : item.result === 1 ? '赢' : '平'}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className={styles.resultBox}>
+              {selectedList.map((item, index) => (
+                <div key={index} className={styles.resultItemBox}>
+                  <div
+                    className={styles.resultItemImg}
+                    style={{
+                      backgroundImage: `url('${item.img}')`,
+                    }}
+                  ></div>
+                </div>
+              ))}
+            </div>
+            <div className={`${styles.heroName} text-secondary`}>你</div>
+            <div className={styles.resultImgBox}>
+              <img
+                className={styles.resultImg}
+                src={
+                  result < 0
+                    ? 'https://resource.blogwxb.cn/longLoad/game-loss.gif'
+                    : result > 0
+                    ? 'https://resource.blogwxb.cn/longLoad/game-win.gif'
+                    : 'https://resource.blogwxb.cn/longLoad/game-level.gif'
+                }
+                alt="result"
+              />
+              <p
+                className={`${
+                  result < 0
+                    ? 'text-error'
+                    : result > 0
+                    ? 'text-success'
+                    : 'text-warning'
+                }`}
+              >
+                {result < 0
+                  ? '你输喽，辣鸡！'
+                  : result > 0
+                  ? '你赢了，算你走运。'
+                  : '什么，居然平手了？'}
+              </p>
+            </div>
           </div>
-        </div>
-        <button className="btn" onClick={goBackHandler}>
-          撤回
-        </button>
-        <button
-          className="btn btn-primary"
-          onClick={() => {
-            onChange(false);
-          }}
-        >
-          投降
-        </button>
+        )}
       </div>
     </Modal>
   );
