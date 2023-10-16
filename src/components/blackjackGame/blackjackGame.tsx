5940c374fb75c9f0c731fa74a188e018579b3432
wxb <superwang9527@163.com>
2023-10-16 08:38:34 +0800

    feat: player card animation finshed

--- src/components/blackjackGame/blackjackGame.tsx
+++ src/components/blackjackGame/blackjackGame.tsx
@@ -1,6 +1,9 @@
+import { useEffect } from 'react';
+import blackjackGameStore from './blackjackGameStore';
+import { observer, useObserver } from 'mobx-react';
 import Modal from '../modal/modal';
 import styles from './blackjackGame.module.less';
-import bg from '@/assets/images/blackjackGameBg.png';
+import classNames from 'classnames';
 
 interface BlackjackGamProps {
   isOpen: boolean;
@@ -9,16 +12,52 @@ interface BlackjackGamProps {
 
 const BlackjackGame = (props: BlackjackGamProps) => {
   const { isOpen, onChange } = props;
+  const playerInfo = useObserver(() => blackjackGameStore.playerInfo);
+  const isOver = useObserver(() => blackjackGameStore.isOver);
+
+  useEffect(() => {
+    if (!isOpen) {
+      return;
+    }
+    blackjackGameStore.resetData();
+  }, [isOpen]);
+
+  const playerAdd = () => {
+    if (isOver) {
+      console.log('game over');
+      return;
+    }
+    blackjackGameStore.addCard(true, blackjackGameStore.getRandomNum());
+  };
+
   return (
     <Modal isOpen={isOpen} className={styles.modalBox} width={600} height={600}>
       <div className={styles.blackjackGameBox}>
-        <div className={styles.testBox}>
-          <div className={styles.front}></div>
-          <div className={styles.black}></div>
+        <div>{JSON.stringify(playerInfo.cardList)}</div>
+        <div className={styles.playerBox}>
+          {playerInfo.cardList.map((item, index) => (
+            <div
+              className={classNames({
+                [styles.cardBox]: true,
+                [styles.rotate]: true,
+              })}
+              key={index}
+            >
+              <div
+                className={classNames([styles.front, styles[`front-${item}`]])}
+              ></div>
+              <div className={styles.black}></div>
+            </div>
+          ))}
+          <div className={classNames([styles.cardBox])}>
+            <div className={styles.black}></div>
+          </div>
         </div>
+
+        <button onClick={playerAdd}>添加</button>
       </div>
     </Modal>
   );
 };
 
-export default BlackjackGame;
+export default observer(BlackjackGame);
---
+++ src/components/blackjackGame/blackjackGameStore.ts
@@ -0,0 +1,93 @@
+import { action, makeAutoObservable, observable, configure } from 'mobx';
+configure({ enforceActions: 'never' });
+
+interface PlayerInfo {
+  cardList: number[];
+  count: number;
+}
+
+class BlackjackGame {
+  playerInfo!: PlayerInfo;
+  computerInfo!: PlayerInfo;
+  isOver!: boolean;
+  isWin!: boolean;
+  constructor() {
+    makeAutoObservable(this);
+    this.resetData();
+  }
+
+  @action
+  resetData() {
+    this.playerInfo = {
+      cardList: [],
+      count: 0,
+    };
+    this.computerInfo = {
+      cardList: [],
+      count: 0,
+    };
+    this.isOver = false;
+    this.isWin = false;
+  }
+
+  @action
+  addCard(isPlayer: boolean, num: number) {
+    if (this.isOver) {
+      return;
+    }
+    if (isPlayer) {
+      this.playerInfo = {
+        cardList: [...this.playerInfo.cardList, num],
+        count: this.playerInfo.count + num,
+      };
+      this.isOver21(isPlayer, this.playerInfo.count);
+      return;
+    }
+    this.computerInfo = {
+      cardList: [...this.computerInfo.cardList, num],
+      count: this.computerInfo.count + num,
+    };
+    this.isOver21(isPlayer, this.computerInfo.count);
+  }
+
+  isOver21(isPlayer: boolean, num: number) {
+    console.log('----', this.playerInfo, this.computerInfo);
+    if (num < 21) {
+      return;
+    }
+    this.isOver = true;
+    this.isWin = num === 21 ? isPlayer : !isPlayer;
+    console.log('--result--', this);
+  }
+
+  @action
+  getRandomNum() {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+
+  @action
+  autoCard() {
+    if (this.isOver) {
+      return;
+    }
+    if (this.computerInfo.count <= this.playerInfo.count) {
+      this.addCard(false, this.getRandomNum());
+      if (this.isOver) {
+        return;
+      }
+      if (this.computerInfo.count <= this.playerInfo.count) {
+        this.autoCard();
+      } else if (this.computerInfo.count > 21) {
+        this.isOver = true;
+        this.isWin = true;
+        console.log('--result--', this);
+      } else {
+        this.isOver = true;
+        this.isWin = false;
+        console.log('--result--', this);
+      }
+    }
+  }
+}
+const blackjackGameStore = new BlackjackGame();
+export default blackjackGameStore;
