bb786af97c177048d9da60178092af63462c262a
wxb <superwang9527@163.com>
2023-10-19 01:48:09 +0800

    feat: caceled route lazy load

--- config/routes.ts
+++ config/routes.ts
@@ -1,21 +1,24 @@
 import { lazy } from 'react';
 import { RouteConfig } from 'react-router-config';
+import Index from '@/pages/index/index';
+import CreateGame from '@/pages/createGame/createGame';
+import Warning from '@/pages/warning/warning';
 
 const routes: RouteConfig[] = [
   {
     exact: true,
     path: '/',
-    component: lazy(() => import('@/pages/index/index')),
+    component: Index,
   },
   {
     exact: true,
     path: '/createGame',
-    component: lazy(() => import('@/pages/createGame/createGame')),
+    component: CreateGame,
   },
   {
     exact: true,
     path: '/warning',
-    component: lazy(() => import('@/pages/warning/warning')),
+    component: Warning,
   },
 ];
 export default routes;
